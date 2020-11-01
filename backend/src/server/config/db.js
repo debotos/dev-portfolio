314eb63f20c44c4bf37804688f3d2df33b738a95
Debotos <debotosdas@gmail.com>
2020-11-01 09:43:30 +0600

    Mongoose updated

--- backend/src/server/config/db.js
+++ backend/src/server/config/db.js
@@ -1,9 +1,14 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const mongoURI = require('./credential/keys').mongoURI;
+const winston = require('winston')
+const mongoose = require('mongoose')
+const mongoURI = require('./credential/keys').mongoURI
 
-module.exports = function() {
-  mongoose
-    .connect(mongoURI, { useNewUrlParser: true })
-    .then(() => winston.info(`Connected to ${mongoURI}...`));
-};
+module.exports = function () {
+	mongoose
+		.connect(mongoURI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+			useCreateIndex: true,
+		})
+		.then(() => winston.info(`Connected to ${mongoURI}...`))
+}
